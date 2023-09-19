f13cebcfca12901c5dad6bd30e9b5a1fd3487e08
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-09-19 10:06:08 +0530

    Add Homepage and Product card

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,21 @@
-const Product = () => {
-  return <div>Product</div>
+const Product = ({ post }) => {
+  return (
+    <div className='flex flex-col'>
+      <div>
+        <p>{post.title}</p>
+      </div>
+      <div>
+        <p>{post.description}</p>
+      </div>
+      <div>
+        <img src={`${post.image}`} className='w-24 h24' />
+      </div>
+      <div>
+        <p>${post.price}</p>
+      </div>
+      <button>Add To Cart</button>
+    </div>
+  )
 }
 
 export default Product
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react'
+import Spinner from '../components/Spinner'
+import Product from '../components/Product'
+
 const Home = () => {
-  return <div>Home</div>
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState([])
+
+  const fetchProductsData = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch('https://fakestoreapi.com/products')
+      const data = await res.json()
+      setPosts(data)
+    } catch (error) {
+      console.log('Error', error)
+      setPosts([])
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchProductsData()
+  }, [])
+
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <div>
+          {posts.map((post) => (
+            <Product key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No Data Found</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Home
