76ade05a5c084b48fe7c642c22f336a6a625ba68
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-09-21 22:02:54 +0530

    add cart page

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,9 @@
 const CartItem = ({ item }) => {
   return (
-    <div className="flex flex-col items-center gap-10 bg-black px-8 py-8">
+    <div className=" w-full flex flex-col items-center gap-8 bg-black px-8 py-6">
       <div className="text-xl">{item.title}</div>
       <div>
-        <img className="w-[400px]" src={item.image} alt="product-image"/>
+        <img className="w-[400px] h-[200px]" src={item.image} alt="product-image"/>
       </div>
       <div className="flex justify-between bg-red-400 w-full px-4 py-2">
         <div>${item.price}</div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,23 +1,42 @@
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import CartItem from '../components/CartItem.jsx'
+import { useEffect, useState } from 'react'
 
 const Cart = () => {
   const { cart } = useSelector(state => state)
+  // const [totalAmount, setTotalAmount] = useState(0)
+  //
+  // useEffect(() => {
+  //   let total = cart.reduce((acc, curValue) => acc + curValue.price)
+  //   setTotalAmount(total)
+  // }, [cart])
 
   return (
-    <div>
+    <div className="w-screen">
       {cart.length > 0 ?
-        <div className="flex justify-between">
+        <div className="flex justify-between bg-red-400">
           {/*CartItems*/}
-          <div>
+          <div className="w-5/12">
             {cart.map(item => (
               <CartItem key={item.id} item={item}/>
             ))}
           </div>
 
           {/*CartSummary*/}
-          <div></div>
+          <div>
+            <div>
+              <div>Your Cart</div>
+              <div>Summary</div>
+              <p>
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
+            <div>
+              {/*<p>Total Amount: {totalAmount}</p>*/}
+              <button>Checkout Now</button>
+            </div>
+          </div>
         </div> :
         <div className="flex flex-col items-center justify-center">
           <h1>Cart is Empty</h1>
